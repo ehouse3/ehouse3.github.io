fa1d9f4d631e2d57f6c2fac0ec915f454693c34b
ehouse3 <54383796+ehouse3@users.noreply.github.com>
2026-03-26 11:26:54 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar></Navbar>
+        {/* <Navbar></Navbar> */}
         {children}
       </body>
     </html>
