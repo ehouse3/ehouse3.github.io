4622d6702231388d6c35c31bcc6055f53bbde919
ehouse3 <houseesh@gmail.com>
2026-03-31 14:02:00 -0400

    Navbar Complete
    Cleaned up effect
    Included effects into global colors
    Finished navbar effect
    Stylizing

--- components/components.tsx
+++ components/components.tsx
@@ -14,17 +14,15 @@ export function HeaderLink(props: HeaderLinkProps): ReactNode {
   // Layered Span elements with hover transition creates multiple shadows,
   // each with a clip path of a triangle that extends below the span to cut off the shadow.
   return (
-    <div className="flex transition-discrete transition-all antialiased">
-      <span className="flex transition-discrete transition-all antialiased shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] hover:shadow-[0rem_2rem_0rem_0rem_rgba(120,123,128,1)] [clip-path:polygon(0%_0%,100%_0%,100%_110%,50%_140%,0_110%)]">
-        <span className="flex transition-discrete transition-all antialiased shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] hover:shadow-[0rem_2rem_0rem_0rem_rgba(102,105,109,1)] [clip-path:polygon(0%_0%,100%_0%,100%_105%,50%_125%,0_105%)]">
-          <Link
-            className="flex items-center transition-discrete transition-all antialiased p-1.5 text-center hover:bg-navbar-hover shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] hover:shadow-[0rem_2rem_0rem_0rem_rgba(77,79,82,1)] [clip-path:polygon(0%_0%,100%_0%,100%_100%,50%_110%,0_100%)]"
-            href={props.href}
-          >
-            {props.children}
-          </Link>
-        </span>
+    <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_110%,50%_140%,0_110%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect2)]">
+      <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_105%,50%_125%,0_105%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect1)]">
+        <Link
+          className="hover:bg-navbar-hover flex items-center p-1.5 text-center text-2xl shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_100%,50%_110%,0_100%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-hover)]"
+          href={props.href}
+        >
+          {props.children}
+        </Link>
       </span>
-    </div>
+    </span>
   );
 }
