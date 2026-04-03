b0007ee69b8e54f7e97d9865e089b383adeb8702
ehouse3 <houseesh@gmail.com>
2026-04-03 14:30:39 -0400

    Navbar Stylizing
    Added border gradient to navbar by wrapping in a div, and adding a bg element at bottom that has a gradient for background
    Adjusted navbar effect duration

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,45 +37,51 @@ function Navbar(): ReactNode {
   // TODO:
   // For mobile screens, convert navbar to a horizontal grid, and stack the home, projects, and socials on top of each other instead of horizontal
   return (
-    // md:overflow-visible and overflow-auto fixs mobile background cutoff on mobile
-    <div className="bg-navbar grid-cols-[1fr_2fr_1fr] overflow-auto md:grid md:overflow-visible">
-      <div id="home" className="hidden items-center justify-start md:flex">
-        <Link href="/">
-          <div className="py-1 pl-3 text-6xl">Euan House</div>
-        </Link>
-      </div>
+    <div className="mb-6">
+      {/* // md:overflow-visible and overflow-auto fixs mobile background cutoff on mobile */}
+      <div className="bg-navbar grid-cols-[1fr_2fr_1fr] overflow-auto md:grid md:overflow-visible">
+        <div id="home" className="hidden items-center justify-start md:flex">
+          <Link href="/">
+            <div className="py-1 pl-3 text-6xl">Euan House</div>
+          </Link>
+        </div>
 
-      <div id="projects" className="flex justify-around">
-        <HeaderLink href={projectPaths.taskManager}>Task Manager</HeaderLink>
-        <HeaderLink href={projectPaths.covidDashboard}>
-          Covid Dashboard
-        </HeaderLink>
-        <HeaderLink href={projectPaths.nittanyBusiness}>
-          Nittany Business Application
-        </HeaderLink>
-        <HeaderLink href={projectPaths.tableTopSimulator}>
-          Tabletop Simulator
-        </HeaderLink>
-      </div>
+        <div id="projects" className="flex justify-around">
+          <HeaderLink href={projectPaths.taskManager}>Task Manager</HeaderLink>
+          <HeaderLink href={projectPaths.covidDashboard}>
+            Covid Dashboard
+          </HeaderLink>
+          <HeaderLink href={projectPaths.nittanyBusiness}>
+            Nittany Business Application
+          </HeaderLink>
+          <HeaderLink href={projectPaths.tableTopSimulator}>
+            Tabletop Simulator
+          </HeaderLink>
+        </div>
 
-      <div id="socials" className="hidden justify-end gap-4 px-4 md:flex">
-        <Link
-          className="hover:bg-navbar-hover flex items-center px-2"
-          href={socialUrls.linkedin}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {<i className="ci ci-linkedin ci-3x"></i>}
-        </Link>
-        <Link
-          className="hover:bg-navbar-hover flex items-center px-2"
-          href={socialUrls.github}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {<i className="ci ci-github-light ci-3x"></i>}
-        </Link>
+        <div id="socials" className="hidden justify-end gap-4 px-4 md:flex">
+          <Link
+            className="hover:bg-navbar-hover flex items-center px-2"
+            href={socialUrls.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {<i className="ci ci-linkedin ci-3x"></i>}
+          </Link>
+          <Link
+            className="hover:bg-navbar-hover flex items-center px-2"
+            href={socialUrls.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {<i className="ci ci-github-light ci-3x"></i>}
+          </Link>
+        </div>
       </div>
+      <div
+        id="border-gradient"
+        className="from-navbar-gradient to-navbar-gradient h-1 w-full bg-linear-to-r via-transparent"
+      ></div>
     </div>
   );
 }
--- components/components.tsx
+++ components/components.tsx
@@ -14,10 +14,10 @@ export function HeaderLink(props: HeaderLinkProps): ReactNode {
   // Layered Span elements with hover transition creates multiple shadows,
   // each with a clip path of a triangle that extends below the span to cut off the shadow.
   return (
-    <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_110%,50%_140%,0_110%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect2)]">
-      <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_105%,50%_125%,0_105%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect1)]">
+    <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-200 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_110%,50%_140%,0_110%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect2)]">
+      <span className="flex shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-200 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_105%,50%_125%,0_105%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-effect1)]">
         <Link
-          className="hover:bg-navbar-hover flex items-center p-1.5 text-center text-2xl shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-300 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_100%,50%_110%,0_100%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-hover)]"
+          className="hover:bg-navbar-hover flex items-center p-1.5 text-center text-2xl shadow-[0rem_0rem_0rem_rgba(0,0,0,0)] transition-all duration-200 ease-in-out [clip-path:polygon(0%_0%,100%_0%,100%_100%,50%_110%,0_100%)] hover:shadow-[0rem_4rem_0rem_0rem_var(--color-navbar-hover)]"
           href={props.href}
         >
           {props.children}
