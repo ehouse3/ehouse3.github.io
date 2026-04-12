7228bd2c4b6606233cac74015c65e233a6bb27da
ehouse3 <houseesh@gmail.com>
2026-04-12 11:28:09 -0400

    Update page.tsx

--- app/projects/covid-dashboard/page.tsx
+++ app/projects/covid-dashboard/page.tsx
@@ -1,3 +1,36 @@
 export default function covidDashboard() {
-  return <div></div>;
+  return (
+    <div>
+      <section data-desc="about"></section>
+      <h2>Covid Dashboard Webapp</h2>
+
+      <p></p>
+
+      <section data-desc="challenges">
+        <h4>Challenges Faced</h4>
+        <p>7 Day Average</p>
+        <p></p>
+        <p>Dynamic graphs and charts</p>
+        <p></p>
+        <p>Data integration</p>
+        <p></p>
+      </section>
+
+      <section data-desc="details">
+        <p>Nextjs</p>
+        <p>
+          For this project, I wanted to expand upon what I learned previously. I
+          prioritized using the modern react library to create a clean and
+          expandable codebase. Combined with Tailwind,
+        </p>
+        <p>
+          I could use component driven architecture which made organization
+          trivial and helped reduce boilerplate code. Storing the state
+          components here helped abstract away
+        </p>
+        <p>Tailwind</p>
+        <p></p>
+      </section>
+    </div>
+  );
 }
